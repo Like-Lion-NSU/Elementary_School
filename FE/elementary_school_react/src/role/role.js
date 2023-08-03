b9b8e10d6eadc38bfa7b182e80e1a5e4aba171f4
imgracehwang <hwangpadark@gmail.com>
2023-08-04 01:08:32 +0900

    [feat]역할 페이지 수정

--- FE/elementary_school_react/src/role/role.js
+++ FE/elementary_school_react/src/role/role.js
@@ -15,14 +15,16 @@ const RolePage = () => {
 
   return (
     <div className="role-page" id="roleE-container">
-      <h1 id="roleE-title">역할을 선택해주세요</h1>
+      <h1 id="roleE-title">역할을 선택해주세요.</h1>
       <div className="role-buttons">
-        <button className="roleE-button" onClick={roleEGoToTeacherPage}>
+        <div className="roleE-button" onClick={roleEGoToTeacherPage}>
           <span role="img" aria-label="Teacher">👩‍🏫</span>
-        </button>
-        <button className="roleE-button" onClick={roleEGoToStudentPage}>
+          <div className='roleE-role'>선생님</div>
+        </div>
+        <div className="roleE-button" onClick={roleEGoToStudentPage}>
           <span role="img" aria-label="Student">👩‍🎓</span>
-        </button>
+          <div className='roleE-role'>학생</div>
+        </div>
       </div>
     </div>
   );
