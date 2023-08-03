bdaf82fafd613e3e1bfeb2ddd9901806bdf43aa9
정재은 <jjyjjy0528@naver.com>
2023-08-03 16:39:15 +0900

    [Docs] 로그인 단위 수정

--- FE/elementary_school_react/src/index.js
+++ FE/elementary_school_react/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LoJLogin from './login/login';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <LoJLogin />
+
 );
 
