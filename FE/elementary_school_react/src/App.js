54b13ebdd7cdaf38b134e9cafca7e9de95bb4944
imgracehwang <hwangpadark@gmail.com>
2023-08-04 02:45:08 +0900

    [feat]연습 종류 페이지

--- FE/elementary_school_react/src/App.js
+++ FE/elementary_school_react/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./login/login";
@@ -8,6 +7,7 @@ import Main from "./main/main";
 import Question from "./question/question";
 import Mypage from "./mypage/mypage";
 import Community from "./community/community";
+import PracticeType from "./kiosk/practiceType";
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
         <Route path="/question" element={<Question />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/community" element={<Community />} />
+        <Route path="/practiceType" element={<PracticeType />} />
       </Routes>
     </>
   );
--- FE/elementary_school_react/src/role/role.js
+++ FE/elementary_school_react/src/role/role.js
@@ -1,30 +1,20 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../css/role.css';
 
 const RolePage = () => {
-  const roleEGoToTeacherPage = () => {
-    navigate('/teacher-page');
-  };
-
-  const roleEGoToStudentPage = () => {
-    navigate('/student-page');
-  };
-
-  const navigate = useNavigate();
-
   return (
     <div className="role-page" id="roleE-container">
       <h1 id="roleE-title">역할을 선택해주세요.</h1>
       <div className="role-buttons">
-        <div className="roleE-button" onClick={roleEGoToTeacherPage}>
+        <Link to="/practiceType" className="roleE-button">
           <span role="img" aria-label="Teacher">👩‍🏫</span>
           <div className='roleE-role'>선생님</div>
-        </div>
-        <div className="roleE-button" onClick={roleEGoToStudentPage}>
+        </Link>
+        <Link to="/" className="roleE-button">
           <span role="img" aria-label="Student">👩‍🎓</span>
           <div className='roleE-role'>학생</div>
-        </div>
+        </Link>
       </div>
     </div>
   );
