5a004d843f5413b5829d2f0892721d28e0e24926
Seo Songhyeon <songhyeon4643@gmail.com>
2023-08-05 17:38:17 +0900

    [Feat] comment service 및 repository 구현

--- BE/school/src/main/java/thisisus/school/post/repository/CommentRepository.java
+++ BE/school/src/main/java/thisisus/school/post/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package thisisus.school.post.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import thisisus.school.post.domain.Comment;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByMemberIdAndIsDeletedIsFalse(Long id);
 }
