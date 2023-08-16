e8cdb45eff86935af7e2f107a4a33f7dc8e60fbe
정재은 <jjyjjy0528@naver.com>
2023-08-17 02:27:28 +0900

    [Feat] 로그아웃 구현, 마이페이지, 사이드바 css

--- FE/elementary_school_react/src/mypage/mypage.js
+++ FE/elementary_school_react/src/mypage/mypage.js
@@ -4,11 +4,7 @@ import My_J_userImg from "./img/userImg.png";
 import { Link } from "react-router-dom";
 import Sidebar from "../sidebar/sidebar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faRightFromBracket,
-  faFileLines,
-  faComments,
-} from "@fortawesome/free-solid-svg-icons";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 
 function MyJMypage() {
@@ -19,132 +15,140 @@ function MyJMypage() {
   const myJshowClick = () => {
     setmyJUserDropShow(true);
   };
-  const [userInfo, setUserInfo] = useState(null);
+  // const [userInfo, setUserInfo] = useState(null);
 
-  useEffect(() => {
-    const fetchMyPage = async () => {
-      try {
-        const accessToken = getCookieValue("accessToken"); // 예시 함수로 쿠키 값 추출
-        console.log("accessToken:", accessToken); // 추가된 부분
+  // useEffect(() => {
+  //   const fetchMyPage = async () => {
+  //     try {
+  //       const accessToken = getCookieValue("accessToken"); // 예시 함수로 쿠키 값 추출
+  //       console.log("accessToken:", accessToken); // 추가된 부분
 
-        const response = await axios.get("/mypage", {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            Accept: "application/json", // JSON 응답을 요청한다고 설정
-          },
-        });
+  //       const response = await axios.get("/mypage", {
+  //         headers: {
+  //           Authorization: `Bearer ${accessToken}`,
+  //           Accept: "application/json", // JSON 응답을 요청한다고 설정
+  //         },
+  //       });
 
-        setUserInfo(response.data);
-        console.log(userInfo);
-      } catch (error) {
-        if (error.response && error.response.status === 401) {
-          try {
-            const refreshToken = getCookieValue("refreshToken"); // 예시 함수로 쿠키 값 추출
+  //       setUserInfo(response.data);
+  //       console.log(userInfo);
+  //     } catch (error) {
+  //       if (error.response && error.response.status === 401) {
+  //         try {
+  //           const refreshToken = getCookieValue("refreshToken"); // 예시 함수로 쿠키 값 추출
 
-            const refreshResponse = await axios.post("/auth/refresh", null, {
-              headers: {
-                Authorization: `Bearer ${refreshToken}`,
-              },
-            });
+  //           const refreshResponse = await axios.post("/auth/refresh", null, {
+  //             headers: {
+  //               Authorization: `Bearer ${refreshToken}`,
+  //             },
+  //           });
 
-            const newAccessToken = refreshResponse.data;
-            // 새로운 AccessToken을 사용하여 다시 마이페이지 정보 요청 등을 수행
-            const refreshedResponse = await axios.get("/mypage", {
-              headers: {
-                Authorization: `Bearer ${newAccessToken}`,
-              },
-            });
+  //           const newAccessToken = refreshResponse.data;
+  //           // 새로운 AccessToken을 사용하여 다시 마이페이지 정보 요청 등을 수행
+  //           const refreshedResponse = await axios.get("/mypage", {
+  //             headers: {
+  //               Authorization: `Bearer ${newAccessToken}`,
+  //             },
+  //           });
 
-            setUserInfo(refreshedResponse.data);
-          } catch (refreshError) {
-            // RefreshToken으로 새로운 AccessToken 발급 실패
-            // 로그아웃 처리 등을 수행
-          }
-        }
-        // Handle other errors
-      }
-    };
+  //           setUserInfo(refreshedResponse.data);
+  //         } catch (refreshError) {
+  //           // RefreshToken으로 새로운 AccessToken 발급 실패
+  //           // 로그아웃 처리 등을 수행
+  //         }
+  //       }
+  //       // Handle other errors
+  //     }
+  //   };
 
-    fetchMyPage();
-  }, []);
+  //   fetchMyPage();
+  // }, []);
 
-  const deleteMember = () => {
-    const accessToken = getCookieValue("accessToken");
-    axios({
-      method: "DELETE",
-      url: "/drop",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    })
-      .then((response) => {
-        console.log(response);
-        window.location.href = "/";
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  // 쿠키 값 추출 함수 예시
-  function getCookieValue(cookieName) {
-    const cookies = document.cookie.split(";");
-    for (const cookie of cookies) {
-      const [name, value] = cookie.trim().split("=");
-      if (name === cookieName) {
-        return value;
-      }
-    }
-  }
+  // const deleteMember = () => {
+  //   const accessToken = getCookieValue("accessToken");
+  //   axios({
+  //     method: "DELETE",
+  //     url: "/drop",
+  //     headers: {
+  //       Authorization: `Bearer ${accessToken}`,
+  //       "Content-Type": "application/x-www-form-urlencoded",
+  //     },
+  //   })
+  //     .then((response) => {
+  //       console.log(response);
+  //       window.location.href = "/";
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // };
+  // const logoutClick = () => {
+  //   axios({
+  //     method: "POST",
+  //     url: "/logout",
+  //   })
+  //     .then((response) => {
+  //       if (response.status === 200) {
+  //         window.location.href = "/";
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // };
+  // // 쿠키 값 추출 함수 예시
+  // function getCookieValue(cookieName) {
+  //   const cookies = document.cookie.split(";");
+  //   for (const cookie of cookies) {
+  //     const [name, value] = cookie.trim().split("=");
+  //     if (name === cookieName) {
+  //       return value;
+  //     }
+  //   }
+  // }
 
   return (
     <div>
       <div className="myJCenter">
-        {userInfo && (
-          <div className="myJContainer">
-            <Sidebar />
-            <div className="myJRole">
-              {userInfo.role === "TEACHER" ? "선생님" : "학생"}
-            </div>
-            <img src={My_J_userImg} />
-            <div className="myJMail">{userInfo.email}</div>
-            <span className="myJPoint">포인트 : {userInfo.point}p</span>
-
-            <div className="myJCommu">
-              <div className="myJButtonTop">
-                <Link to="/posting">
-                  <FontAwesomeIcon icon={faFileLines} /> 내가 쓴 게시글
-                </Link>
-              </div>
-              <div className="myJButtonMiddle">
-                <Link to="/comment">
-                  <FontAwesomeIcon icon={faComments} /> 내가 쓴 댓글
-                </Link>
-              </div>
-              <div className="myJLogout">
-                <Link to="/">
-                  <FontAwesomeIcon icon={faRightFromBracket} /> 로그아웃
-                </Link>
-              </div>
-            </div>
-            <span className="myJDrop" onClick={myJshowClick}>
-              회원 탈퇴
-            </span>
+        {/* {userInfo && ( */}
+        <div className="myJContainer">
+          <Sidebar />
+          <div className="myJRole">
+            {/* {userInfo.role === "TEACHER" ? "선생님" : "학생"} */}
           </div>
-        )}
+          <img className="myJUserImg" src={My_J_userImg} />
+          <div className="myJMail">{/*userInfo.email*/}1</div>
+          <span className="myJPoint">포인트 : {/*userInfo.point*/ 1}p</span>
+
+          <div className="myJCommu">
+            <div className="myJButtonTop">
+              <Link to="/posting">내가 쓴 게시글</Link>
+            </div>
+            <div className="myJButtonMiddle">
+              <Link to="/comment">내가 쓴 댓글</Link>
+            </div>
+            <div className="myJLogout" /* onClick={logoutClick}*/>
+              <FontAwesomeIcon icon={faRightFromBracket} /> 로그아웃
+            </div>
+          </div>
+          <span className="myJDrop" onClick={myJshowClick}>
+            회원 탈퇴
+          </span>
+        </div>
+        {/* )} */}
 
         {myJuserDropShow && (
           <div className="myJCenter myJdropUser">
             <div className="myJuserDropContent">
               <div>
-                <h1>국민학교를 자퇴하시겠습니까?</h1>
+                <h1 className="myJUserDropTitle">
+                  국민학교를 자퇴하시겠습니까?
+                </h1>
               </div>
               <div className="myJdropFlex">
                 <div
                   className="myJdropButton"
-                  style={{ backgroundColor: "#4682A9", color: "white" }}
-                  onClick={deleteMember}
+                  /*onClick={deleteMember}*/
                 >
                   예
                 </div>
--- FE/elementary_school_react/src/sidebar/sidebar.js
+++ FE/elementary_school_react/src/sidebar/sidebar.js
@@ -1,38 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../css/sidebar.css";
-import Lo_J_Logo from './img/logo.png'
+import Lo_J_Logo from "./img/logo.png";
+import axios from "axios";
 
 const Sidebar = () => {
+  const logoutClick = async () => {
+    try {
+      const response = await axios.post("/logout");
+      if (response.status === 200) {
+        window.location.href = "/";
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-      <div className="sidebar">
-        <ul className="sidebarE-list">
-          <li>
-            <Link to="/home"><img src={Lo_J_Logo} className='logologo-E' alt='로고'/></Link>
-          </li>
-          <li>
-            <Link to="/community">소통해요</Link>
-          </li>
-          <li>
-            <Link to="/question">질문해요</Link>
-          </li>
-          <li>
-            <Link to="/practiceType">연습하기</Link>
-          </li>
-          <li>
-            <Link to="/policy">정책정보</Link>
-          </li>
-          <li>
-            <Link to="/mypage">나의정보</Link>
-          </li>
-          <li>
-            <Link to="/shop">매점가기</Link>
-          </li>
-          <li>
-            <Link to="/">로그아웃</Link>
-          </li>
-        </ul>
-      </div>
+    <div className="sidebar">
+      <ul className="sidebarE-list">
+        <li>
+          <Link to="/home">
+            <img src={Lo_J_Logo} className="logologo-E" alt="로고" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/community">소통해요</Link>
+        </li>
+        <li>
+          <Link to="/question">질문해요</Link>
+        </li>
+        <li>
+          <Link to="/practiceType">연습하기</Link>
+        </li>
+        <li>
+          <Link to="/policy">정책정보</Link>
+        </li>
+        <li>
+          <Link to="/mypage">나의정보</Link>
+        </li>
+        <li>
+          <Link to="/shop">매점가기</Link>
+        </li>
+        <li>
+          <div className="sideCursorPointer" onClick={logoutClick}>
+            로그아웃
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 };
 
