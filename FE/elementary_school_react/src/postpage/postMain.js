ad71878609a270aa9106c85216857b7e83254549
JCH <ckdgusjang@naver.com>
2023-08-17 01:34:59 +0900

    [Fix]게시물 수정하기 하려는데 ..

--- FE/elementary_school_react/src/postpage/postMain.js
+++ FE/elementary_school_react/src/postpage/postMain.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import '../css/post.css';
+import React from "react";
+import "../css/post.css";
+import axios from "axios";
 
-const PostMain = ({ content, likes, views }) => {
-
+const PostMain = ({ content, likes, views, category, postId }) => {
+  const UpdatePost = () => {
+    axios({
+      method: "PUT",
+      url: `/post/${category}/${postId}`,
+      data: {
+        id: postId,
+      },
+    });
+  };
   return (
     <div className="post-main">
-      <div className="post-content">
-        {content}
-      </div>
+      <div className="post-content">{content}</div>
       <div className="post-meta">
         <p>추천수: {likes}</p>
         <p>조회수: {views}</p>
+        <p>수정</p>
       </div>
     </div>
   );
