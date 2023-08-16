b99b22cd2406c8f2401954483108439fdbc1a143
JCH <ckdgusjang@naver.com>
2023-08-16 21:45:48 +0900

    [Refactor]은혜

--- FE/elementary_school_react/src/board/boardTable.js
+++ FE/elementary_school_react/src/board/boardTable.js
@@ -1,69 +1,42 @@
-import React from 'react';
-import '../css/boardTable.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../css/boardTable.css";
+import { Link } from "react-router-dom";
 
 function BoardTable({ posts }) {
-  return (
-    <div>
-      <div className="board-table-container">
-        <table className="board-table">
-          <thead>
-            <tr>
-              <th>제목</th>
-              <th>작성자</th>
-              <th>날짜</th>
-              <th>조회수</th>
-            </tr>
-          </thead>
-          <tbody className="board-table-body">
-            {posts.map(post => (
-              <tr key={post.id} onClick={() => window.location.href = `/post/${post.id}`} style={{ cursor: 'pointer' }}>
-                <td>{post.title}</td>
-                <td>{post.author}</td>
-                <td>{post.date}</td>
-                <td>{post.views}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <Link to="/write" className="board-write-button">글쓰기</Link>
-    </div>
-  );
+    return (
+        <div>
+            <div className='board-table-container'>
+                <table className='board-table'>
+                    <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>날짜</th>
+                        <th>조회수</th>
+                    </tr>
+                    </thead>
+                    <tbody className='board-table-body'>
+                    {posts.map((post) => (
+                        <tr key={post.id}>
+                            <td>
+                                <Link to={`/post/${post.category}/${post.id}`}>{post.id}</Link>
+                            </td>
+                            <td>{post.title}</td>
+                            <td>{post.memberId}</td>
+                            <td>{post.updateAt}</td>
+                            <td>{post.viewCount}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+
+                </table>
+            </div>
+            <Link to='/writePost' className='board-write-button'>
+                글쓰기
+            </Link>
+        </div>
+    );
 }
 
 export default BoardTable;
-
-// import React from 'react';
-
-// function BoardTable({ boardTitle, posts }) {
-//   return (
-//     <div>
-//       <h2>{boardTitle} 게시판</h2>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>ID</th>
-//             <th>제목</th>
-//             <th>글쓴이</th>
-//             <th>작성일</th>
-//             <th>조회수</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {posts.map(post => (
-//             <tr key={post.id}>
-//               <td>{post.id}</td>
-//               <td>{post.title}</td>
-//               <td>{post.author}</td>
-//               <td>{post.date}</td>
-//               <td>{post.views}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-
-// export default BoardTable;
--- FE/elementary_school_react/src/postpage/postPage.js
+++ FE/elementary_school_react/src/postpage/postPage.js
@@ -1,150 +1,51 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import PostHeader from './postHeader';
-import PostMain from './postMain';
-import PostFooter from './postFooter';
-import PostLike from './postLike';
-import Sidebar from '../sidebar/sidebar';
-import '../css/post.css';
-
-const dummyPosts = [
-  {
-    id: 1,
-    title: '테스트 게시물 1 제목입니다.',
-    authorEmail: 'test1@example.com',
-    content: '테스트 게시물 1 내용입니다.',
-    likes: 10,
-    views: 50,
-    comments: [],
-  },
-  {
-    id: 2,
-    title: '테스트 게시물 2 제목입니다.',
-    authorEmail: 'test2@example.com',
-    content: '테스트 게시물 2 내용입니다.',
-    likes: 15,
-    views: 30,
-    comments: [],
-  },
-  // 다른 더미 데이터도 추가
-];
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import PostHeader from "./postHeader";
+import PostMain from "./postMain";
+import PostFooter from "./postFooter";
+import PostLike from "./postLike";
+import Sidebar from "../sidebar/sidebar";
+import axios from "axios";
+import "../css/post.css";
 
 const PostPage = () => {
-  const { postId } = useParams();
+    const { category, postId } = useParams();
+    const [selectedPost, setSelectedPost] = useState(null);
 
-  const selectedPost = dummyPosts.find(post => post.id === Number(postId));
+    useEffect(() => {
+        async function fetchPostData() {
+            try {
+                const response = await axios.get(`/post/${category}/${postId}`); // URL 형식 변경
+                console.log(response.data);
+                setSelectedPost(response.data); // setPost 대신 setSelectedPost 사용
+            } catch (error) {
+                console.error('게시물 데이터를 가져오는 중 에러가 발생했습니다.', error);
+                setSelectedPost(null);
+            }
+        }
 
-  if (!selectedPost) {
-    return <div>게시물이 없습니다.</div>;
-  }
+        fetchPostData();
+    }, [category, postId]);
 
-  return (
-    <div>
-      <Sidebar />
-      <div className="post-container">
-        <PostHeader title={selectedPost.title} authorEmail={selectedPost.authorEmail} />
-        <PostMain content={selectedPost.content} likes={selectedPost.likes} views={selectedPost.views} />
-        <PostLike postId={selectedPost.id} initialLikes={selectedPost.likes} />
-        <PostFooter comments={selectedPost.comments} />
-      </div>
-    </div>
-  );
+    if (!selectedPost) {
+        return <div>게시물을 불러오는 중 오류가 발생했습니다.</div>;
+    }
+
+    const { title, memberId, content, likeCount, viewCount, comments } = selectedPost;
+
+    return (
+        <div>
+            <Sidebar />
+            <div className="post-container">
+                <PostHeader title={title} authorEmail={memberId} />
+                <PostMain content={content} likes={likeCount} views={viewCount} />
+                {category === "소통하기" || category === "질문하기" ? (
+                    <PostLike postId={postId} /*initialLikes={selectedPost.likes}*/ />
+                ) : null}
+                <PostFooter comments={comments} />
+            </div>
+        </div>
+    );
 };
 
 export default PostPage;
-
-// import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import PostHeader from './postHeader';
-// import PostMain from './postMain';
-// import PostFooter from './postFooter';
-// import PostLike from './postLike';
-// import Sidebar from '../sidebar/sidebar';
-// import axios from 'axios'; // axios 추가
-// import '../css/post.css';
-
-// const PostPage = () => {
-//   const { postId } = useParams();
-//   const [selectedPost, setSelectedPost] = useState(null);
-
-//   useEffect(() => {
-//     async function fetchPostData() {
-//       try {
-//         const response = await axios.get(`/api/post/${postId}`); // API 엔드포인트에 맞게 수정
-//         setSelectedPost(response.data);
-//       } catch (error) {
-//         console.error('게시물 데이터를 가져오는 중 에러가 발생했습니다.', error);
-//         setSelectedPost(null);
-//       }
-//     }
-
-//     fetchPostData();
-//   }, [postId]);
-
-//   if (!selectedPost) {
-//     return <div>게시물을 불러오는 중 오류가 발생했습니다.</div>;
-//   }
-
-//   return (
-//     <div>
-//       <Sidebar />
-//       <div className="post-container">
-//         <PostHeader title={selectedPost.title} authorEmail={selectedPost.authorEmail} />
-//         <PostMain content={selectedPost.content} likes={selectedPost.likes} views={selectedPost.views} />
-//         <PostLike postId={selectedPost.id} initialLikes={selectedPost.likes} />
-//         <PostFooter comments={selectedPost.comments} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PostPage;
-
-// import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import PostHeader from './postHeader';
-// import PostMain from './postMain';
-// import PostFooter from './postFooter';
-// import PostLike from './postLike';
-// import Sidebar from '../sidebar/sidebar';
-// import axios from 'axios';
-// import '../css/post.css';
-
-// const PostPage = () => {
-//   const { postId } = useParams();
-//   const [selectedPost, setSelectedPost] = useState(null);
-
-//   useEffect(() => {
-//     async function fetchPostData() {
-//       try {
-//         const response = await axios.get(`/api/post/${postId}`);
-//         setSelectedPost(response.data);
-//       } catch (error) {
-//         console.error('게시물 데이터를 가져오는 중 에러가 발생했습니다.', error);
-//         setSelectedPost(null);
-//       }
-//     }
-
-//     fetchPostData();
-//   }, [postId]);
-
-//   if (!selectedPost) {
-//     return <div>게시물을 불러오는 중 오류가 발생했습니다.</div>;
-//   }
-
-//   return (
-//     <div>
-//       <Sidebar />
-//       <div className="post-container">
-//         <PostHeader title={selectedPost.title} authorEmail={selectedPost.authorEmail} />
-//         <PostMain content={selectedPost.content} likes={selectedPost.likes} views={selectedPost.views} />
-//         {selectedPost.category === '소통해요' || selectedPost.category === '질문해요' ? (
-//           <PostLike postId={selectedPost.id} initialLikes={selectedPost.likes} />
-//         ) : null}
-//         <PostFooter comments={selectedPost.comments} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PostPage;
