96f23f12e7971e8191a032ffd2f83a6399fd96bb
JCH <ckdgusjang@naver.com>
2023-08-16 21:13:51 +0900

    [Refactor]main합치기

--- FE/elementary_school_react/src/main/main.js
+++ FE/elementary_school_react/src/main/main.js
@@ -1,43 +1,111 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../css/main.css";
 import Sidebar from "../sidebar/sidebar";
+import axios from "axios";
 
 const Main = () => {
+  const [userInfo, setUserInfo] = useState(null);
+  useEffect(() => {
+    const tokenMain = async () => {
+      try {
+        const accessToken = getCookieValue("accessToken"); // 예시 함수로 쿠키 값 추출
+        console.log("accessToken:", accessToken); // 추가된 부분
+
+        const response = await axios.get("/home", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            Accept: "application/json", // JSON 응답을 요청한다고 설정
+          },
+        });
+
+        setUserInfo(response.data);
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          try {
+            const refreshToken = getCookieValue("refreshToken"); // 예시 함수로 쿠키 값 추출
+
+            const refreshResponse = await axios
+              .post("/auth/refresh", null, {
+                headers: {
+                  Authorization: `Bearer ${refreshToken}`,
+                },
+              })
+              .then((result) => {
+                if (result === "403") {
+                  window.location.href = "/403";
+                }
+              });
+
+            const newAccessToken = refreshResponse.data;
+            // 새로운 AccessToken을 사용하여 다시 마이페이지 정보 요청 등을 수행
+            const refreshedResponse = await axios.get("/home", {
+              headers: {
+                Authorization: `Bearer ${newAccessToken}`,
+              },
+            });
+
+            setUserInfo(refreshedResponse.data);
+          } catch (refreshError) {
+            // RefreshToken으로 새로운 AccessToken 발급 실패
+            // 로그아웃 처리 등을 수행
+          }
+        }
+        // Handle other errors
+      }
+    };
+
+    tokenMain();
+  }, []);
+
+  // 쿠키 값 추출 함수 예시
+  function getCookieValue(cookieName) {
+    const cookies = document.cookie.split(";");
+    for (const cookie of cookies) {
+      const [name, value] = cookie.trim().split("=");
+      if (name === cookieName) {
+        return value;
+      }
+    }
+  }
   return (
     <div className="main-page" id="mainE-container">
-      <Sidebar />
-      <h1 id="mainE-title">환영합니다</h1>
-      <div className="main-buttons-wrapper">
-        <div className="main-buttons">
-          <Link to="/community" className="mainE-button">
-            <span role="img" aria-label="communication">
-              📣
-            </span>
-            <div className="mainE-role">소통해요</div>
-          </Link>
-          <Link to="/question" className="mainE-button">
-            <span role="img" aria-label="qna">
-              📝
-            </span>
-            <div className="mainE-role">질문해요</div>
-          </Link>
+      {userInfo && (
+        <div>
+          <Sidebar />
+          <h1 id="mainE-title">환영합니다</h1>
+          <div className="main-buttons-wrapper">
+            <div className="main-buttons">
+              <Link to="/community" className="mainE-button">
+                <span role="img" aria-label="communication">
+                  📣
+                </span>
+                <div className="mainE-role">소통해요</div>
+              </Link>
+              <Link to="/question" className="mainE-button">
+                <span role="img" aria-label="qna">
+                  📝
+                </span>
+                <div className="mainE-role">질문해요</div>
+              </Link>
+            </div>
+            <div className="main-buttons">
+              <Link to="/practiceType" className="mainE-button">
+                <span role="img" aria-label="practice">
+                  📱
+                </span>
+                <div className="mainE-role">연습하기</div>
+              </Link>
+              <Link to="/policy" className="mainE-button">
+                <span role="img" aria-label="information">
+                  🗞️
+                </span>
+                <div className="mainE-role">정책정보</div>
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="main-buttons">
-          <Link to="/practiceType" className="mainE-button">
-            <span role="img" aria-label="practice">
-              📱
-            </span>
-            <div className="mainE-role">연습하기</div>
-          </Link>
-          <Link to="/policy" className="mainE-button">
-            <span role="img" aria-label="information">
-              🗞️
-            </span>
-            <div className="mainE-role">정책정보</div>
-          </Link>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
