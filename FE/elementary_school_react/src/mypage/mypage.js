8709dfff492ced771480db95b2c1a27cbb34d3a4
JCH <ckdgusjang@naver.com>
2023-08-16 21:16:29 +0900

    [Refactor]마이페이지

--- FE/elementary_school_react/src/mypage/mypage.js
+++ FE/elementary_school_react/src/mypage/mypage.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../css/mypage.css";
 import My_J_userImg from "./img/userImg.png";
 import { Link } from "react-router-dom";
 import Sidebar from "../sidebar/sidebar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import {
+  faRightFromBracket,
+  faFileLines,
+  faComments,
+} from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 
 function MyJMypage() {
@@ -15,6 +19,55 @@ function MyJMypage() {
   const myJshowClick = () => {
     setmyJUserDropShow(true);
   };
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchMyPage = async () => {
+      try {
+        const accessToken = getCookieValue("accessToken"); // 예시 함수로 쿠키 값 추출
+        console.log("accessToken:", accessToken); // 추가된 부분
+
+        const response = await axios.get("/mypage", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            Accept: "application/json", // JSON 응답을 요청한다고 설정
+          },
+        });
+
+        setUserInfo(response.data);
+        console.log(userInfo);
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          try {
+            const refreshToken = getCookieValue("refreshToken"); // 예시 함수로 쿠키 값 추출
+
+            const refreshResponse = await axios.post("/auth/refresh", null, {
+              headers: {
+                Authorization: `Bearer ${refreshToken}`,
+              },
+            });
+
+            const newAccessToken = refreshResponse.data;
+            // 새로운 AccessToken을 사용하여 다시 마이페이지 정보 요청 등을 수행
+            const refreshedResponse = await axios.get("/mypage", {
+              headers: {
+                Authorization: `Bearer ${newAccessToken}`,
+              },
+            });
+
+            setUserInfo(refreshedResponse.data);
+          } catch (refreshError) {
+            // RefreshToken으로 새로운 AccessToken 발급 실패
+            // 로그아웃 처리 등을 수행
+          }
+        }
+        // Handle other errors
+      }
+    };
+
+    fetchMyPage();
+  }, []);
+
   const deleteMember = () => {
     const accessToken = getCookieValue("accessToken");
     axios({
@@ -24,8 +77,16 @@ function MyJMypage() {
         Authorization: `Bearer ${accessToken}`,
         "Content-Type": "application/x-www-form-urlencoded",
       },
-    }).then((res) => {});
+    })
+      .then((response) => {
+        console.log(response);
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
+
   // 쿠키 값 추출 함수 예시
   function getCookieValue(cookieName) {
     const cookies = document.cookie.split(";");
@@ -39,32 +100,39 @@ function MyJMypage() {
 
   return (
     <div>
-      <Sidebar />
-
       <div className="myJCenter">
-        <div className="myJContainer">
-          <div className="myJRole">학생</div>
-          <img src={My_J_userImg} />
-          <div className="myJMail">hwangpadark@gmail.com</div>
-          <span className="myJPoint">포인트 : 1004p</span>
+        {userInfo && (
+          <div className="myJContainer">
+            <Sidebar />
+            <div className="myJRole">
+              {userInfo.role === "TEACHER" ? "선생님" : "학생"}
+            </div>
+            <img src={My_J_userImg} />
+            <div className="myJMail">{userInfo.email}</div>
+            <span className="myJPoint">포인트 : {userInfo.point}p</span>
 
-          <div className="myJCommu">
-            <div className="myJButtonTop">
-              <Link to="/posting">내가 쓴 게시글</Link>
+            <div className="myJCommu">
+              <div className="myJButtonTop">
+                <Link to="/posting">
+                  <FontAwesomeIcon icon={faFileLines} /> 내가 쓴 게시글
+                </Link>
+              </div>
+              <div className="myJButtonMiddle">
+                <Link to="/comment">
+                  <FontAwesomeIcon icon={faComments} /> 내가 쓴 댓글
+                </Link>
+              </div>
+              <div className="myJLogout">
+                <Link to="/">
+                  <FontAwesomeIcon icon={faRightFromBracket} /> 로그아웃
+                </Link>
+              </div>
             </div>
-            <div className="myJButtonMiddle">
-              <Link to="/comments">내가 쓴 댓글</Link>
-            </div>
-            <div className="myJLogout">
-              <Link to="/">
-                <FontAwesomeIcon icon={faRightFromBracket} /> 로그아웃
-              </Link>
-            </div>
+            <span className="myJDrop" onClick={myJshowClick}>
+              회원 탈퇴
+            </span>
           </div>
-          <span className="myJDrop" onClick={myJshowClick}>
-            회원 탈퇴
-          </span>
-        </div>
+        )}
 
         {myJuserDropShow && (
           <div className="myJCenter myJdropUser">
@@ -74,9 +142,8 @@ function MyJMypage() {
               </div>
               <div className="myJdropFlex">
                 <div
-                  to="/"
                   className="myJdropButton"
-                  style={{ backgroundColor: "#91C8E4" }}
+                  style={{ backgroundColor: "#4682A9", color: "white" }}
                   onClick={deleteMember}
                 >
                   예
