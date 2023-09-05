4229822900830c8e7d0c9cf9bc0e8331761ba15e
JCH <ckdgusjang@naver.com>
2023-09-05 22:06:20 +0900

    [Refactor]백엔드 커밋잘해라이제

--- BE/school/src/main/java/thisisus/school/member/etc/MemberNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package thisisus.school.member.etc;
-
-public class MemberNotFoundException extends RuntimeException {
-    public MemberNotFoundException(String message) {
-        super(message);
-    }
-}
