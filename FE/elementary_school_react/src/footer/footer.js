d7adfe52257f73a46f66c2c91428174376de96f8
JCH <ckdgusjang@naver.com>
2023-09-17 14:27:54 +0900

    [fix]바닥글 수정

--- FE/elementary_school_react/src/footer/footer.js
+++ FE/elementary_school_react/src/footer/footer.js
@@ -1,12 +1,17 @@
 import React from "react";
-import likelion from "./whitelogo.png"
-import "../css/footer.css"
+import likelion from "./whitelogo.png";
+import "../css/footer.css";
 
-const Footer=()=>{
-    return(
-        <div className="Footer">
-            <div className="JTable"><img src={likelion} className="Jlogo"/>&nbsp;&nbsp;<b>소속</b> 남서울대학교&nbsp;&nbsp;<b>팀명</b> 이게우리야&nbsp;&nbsp;<b>Back-End</b> 권영태 서송현&nbsp;&nbsp;<b>Front-End</b> 장창현 황은혜 정재은</div>
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div className="JTable">
+        <img src={likelion} className="Jlogo" />
+        &nbsp;&nbsp;<b>소속</b> 남서울대학교&nbsp;&nbsp;<b>팀명</b>{" "}
+        이게우리야&nbsp;&nbsp;<b>Back-End</b> 권영태 서송현 박민서&nbsp;&nbsp;
+        <b>Front-End</b> 장창현 황은혜 정재은
       </div>
-    )
-}
+    </div>
+  );
+};
 export default Footer;
