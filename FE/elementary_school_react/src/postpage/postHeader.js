7f677131094df6e3eb9dc30d3649042a20f43c10
JCH <ckdgusjang@naver.com>
2023-09-20 23:43:40 +0900

    [fix] 소켓부분

--- FE/elementary_school_react/src/postpage/postHeader.js
+++ FE/elementary_school_react/src/postpage/postHeader.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../css/post.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const PostHeader = ({
   title,
@@ -9,15 +10,35 @@ const PostHeader = ({
   currentMemberId,
   postMemberId,
 }) => {
+  const MakeRoom = async () => {
+    const accessToken = getCookieValue("accessToken");
+    try {
+      const response = await axios({
+        method: "POST",
+        url: `/v1/chat/${postMemberId}`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      window.location.href = `/chat/${response.data.roomId}`;
+    } catch (error) {
+      console.log("방만들다가 에러", error);
+    }
+  };
+  function getCookieValue(cookieName) {
+    const cookies = document.cookie.split(";");
+    for (const cookie of cookies) {
+      const [name, value] = cookie.trim().split("=");
+      if (name === cookieName) {
+        return value;
+      }
+    }
+  }
   return (
     <div className="post-header">
       <h2>{title}</h2>
       <p>{authorEmail}</p>
-      {authorEmail !== currentEmail && (
-        <Link to={`/chat/${currentMemberId}/${postMemberId}`}>
-          <button>채팅</button>
-        </Link>
-      )}
+      {authorEmail !== currentEmail && <button onClick={MakeRoom}>채팅</button>}
     </div>
   );
 };
