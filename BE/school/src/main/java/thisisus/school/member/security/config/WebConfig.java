ac7bdaa3c7cf6e3100fd0e5335aa1d936d8900bc
dudxo <father8475@naver.com>
2023-09-20 09:50:11 +0900

    [Fix] WebConfig Bean 등록

--- BE/school/src/main/java/thisisus/school/member/security/config/WebConfig.java
+++ BE/school/src/main/java/thisisus/school/member/security/config/WebConfig.java
@@ -1,9 +1,13 @@
 package thisisus.school.member.security.config;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
