e6309efa9eaa6dd4bf9eccb3acf13d90248e4db3
shyeon4643 <songhyeon4643@gmail.com>
2024-03-27 01:46:51 +0900

    style : 불필요한 파일 제거

--- BE/school/src/main/java/thisisus/school/member/security/etc/OAuthProcessingException.java
+++
@@ -1,8 +0,0 @@
-package thisisus.school.member.security.etc;
-
-public class OAuthProcessingException extends RuntimeException{
-
-    public OAuthProcessingException(String message) {
-        super(message);
-    }
-}
--- BE/school/src/main/java/thisisus/school/socket/repository/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package thisisus.school.socket.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import thisisus.school.socket.model.ChatRoom;
-
-public interface RoomRepository extends JpaRepository<ChatRoom, Long> {
-    ChatRoom findByRoomId(String id);
-}
