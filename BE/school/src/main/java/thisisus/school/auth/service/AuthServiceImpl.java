af8532ee594627c5d6462bf64aebb79adff856cc
shyeon4643 <songhyeon4643@gmail.com>
2024-06-14 01:13:03 +0900

    fix : auth 관련 예외 메시지 수정

--- BE/school/src/main/java/thisisus/school/auth/service/AuthServiceImpl.java
+++ BE/school/src/main/java/thisisus/school/auth/service/AuthServiceImpl.java
@@ -1,8 +1,9 @@
 package thisisus.school.auth.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 import thisisus.school.auth.dto.request.SignUpRequest;
 import thisisus.school.auth.dto.response.AuthResponse;
 import thisisus.school.auth.dto.response.IdTokenResponse;
@@ -12,7 +13,6 @@ import thisisus.school.auth.exception.NotFoundEmailException;
 import thisisus.school.auth.infrastructure.CreateToken;
 import thisisus.school.auth.infrastructure.kakao.KakaoProperties;
 import thisisus.school.auth.infrastructure.kakao.KakaoTokenInfoResponse;
-import thisisus.school.auth.infrastructure.kakao.RequestKakaoMemberInfoClient;
 import thisisus.school.auth.infrastructure.kakao.RequestKakaoOauthClient;
 import thisisus.school.auth.processor.LoginByIdTokenProcessor;
 import thisisus.school.member.domain.Member;
@@ -23,49 +23,48 @@ import thisisus.school.member.repository.MemberRepository;
 @Transactional(readOnly = true)
 public class AuthServiceImpl implements AuthService {
 
-    private final MemberRepository memberRepository;
-    private final RequestKakaoOauthClient requestKakaoOauthClient;
-    private final RequestKakaoMemberInfoClient requestKakaoMemberInfoClient;
-    private final LoginByIdTokenProcessor loginByIdTokenProcessor;
-    private final KakaoProperties kakaoProperties;
-    private final CreateToken createToken;
+	private final MemberRepository memberRepository;
+	private final RequestKakaoOauthClient requestKakaoOauthClient;
+	private final LoginByIdTokenProcessor loginByIdTokenProcessor;
+	private final KakaoProperties kakaoProperties;
+	private final CreateToken createToken;
 
-    @Override
-    public IdTokenResponse getIdToken(String code) {
-        KakaoTokenInfoResponse kakaoTokenInfoResponse = requestKakaoOauthClient.getToken(
-            kakaoProperties.getClientId(),
-            kakaoProperties.getRedirectUrl(),
-            code);
-        return new IdTokenResponse(kakaoTokenInfoResponse.getIdToken());
-    }
+	@Override
+	public IdTokenResponse getIdToken(String code) {
+		KakaoTokenInfoResponse kakaoTokenInfoResponse = requestKakaoOauthClient.getToken(
+			kakaoProperties.getClientId(),
+			kakaoProperties.getRedirectUrl(),
+			code);
+		return new IdTokenResponse(kakaoTokenInfoResponse.getIdToken());
+	}
 
-    @Override
-    public AuthResponse login(String idToken) {
-        MemberInfoFromIdToken memberInfoFromIdToken = loginByIdTokenProcessor.getMemberInfoFromIdToken(idToken);
-        if (validateEmail(memberInfoFromIdToken.getEmail())) {
-            Member member = memberRepository.findByEmail(memberInfoFromIdToken.getEmail());
-            return createToken.createAccessToken(member);
-        } else {
-            throw new NotFoundEmailException();
-        }
-    }
+	@Override
+	public AuthResponse login(String idToken) {
+		MemberInfoFromIdToken memberInfoFromIdToken = loginByIdTokenProcessor.getMemberInfoFromIdToken(idToken);
+		if (validateEmail(memberInfoFromIdToken.getEmail())) {
+			Member member = memberRepository.findByEmail(memberInfoFromIdToken.getEmail());
+			return createToken.createAccessToken(member);
+		} else {
+			throw new NotFoundEmailException();
+		}
+	}
 
-    @Override
-    @Transactional
-    public AuthResponse signUp(String idToken, SignUpRequest signUpRequest) {
-        MemberInfoFromIdToken memberInfoFromIdToken = loginByIdTokenProcessor.getMemberInfoFromIdToken(idToken);
-        if (!validateEmail(memberInfoFromIdToken.getEmail())) {
-            Member member = signUpRequest.toEntity(memberInfoFromIdToken.getEmail());
-            memberRepository.save(member);
-            AuthResponse authResponse = createToken.createAccessToken(member);
-            member.setRefreshToken(authResponse.getRefreshToken());
-            return authResponse;
-        } else {
-            throw new AlreadyRegisteredEmailException();
-        }
-    }
+	@Override
+	@Transactional
+	public AuthResponse signUp(String idToken, SignUpRequest signUpRequest) {
+		MemberInfoFromIdToken memberInfoFromIdToken = loginByIdTokenProcessor.getMemberInfoFromIdToken(idToken);
+		if (!validateEmail(memberInfoFromIdToken.getEmail())) {
+			Member member = signUpRequest.toEntity(memberInfoFromIdToken.getEmail());
+			memberRepository.save(member);
+			AuthResponse authResponse = createToken.createAccessToken(member);
+			member.setRefreshToken(authResponse.getRefreshToken());
+			return authResponse;
+		} else {
+			throw new AlreadyRegisteredEmailException();
+		}
+	}
 
-    private Boolean validateEmail(String email) {
-        return memberRepository.existsByEmail(email);
-    }
+	private Boolean validateEmail(String email) {
+		return memberRepository.existsByEmail(email);
+	}
 }
