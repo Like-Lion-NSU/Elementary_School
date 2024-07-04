fa17746b2d02097f26533c6c27e2dff47994318f
JCH <ckdgusjang@naver.com>
2024-07-05 00:29:14 +0900

    refactor : 좋아요 기능 엔터티 구현, Post 엔터티, Member 엔터티와 단방향 연관관계 설정, N:1

---
+++ BE/school/src/main/java/thisisus/school/post/domain/Post_Like.java
@@ -0,0 +1,26 @@
+package thisisus.school.post.domain;
+
+import lombok.*;
+import thisisus.school.member.domain.Member;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post_Like {
+    @Id
+    @GeneratedValue
+    @Column(name = "post_like_id")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
