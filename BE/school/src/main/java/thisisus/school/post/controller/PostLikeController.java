acb41067997d695056be8515c00831ac63022ccc
JCH <ckdgusjang@naver.com>
2024-07-25 21:13:36 +0900

    fix : 게시물 좋아요 기능 관련 api수정

--- BE/school/src/main/java/thisisus/school/post/controller/PostLikeController.java
+++ BE/school/src/main/java/thisisus/school/post/controller/PostLikeController.java
@@ -8,19 +8,19 @@ import thisisus.school.post.service.PostLikeService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/post/like")
+@RequestMapping("/api/post")
 public class PostLikeController {
 
     private final PostLikeService postLikeService;
 
-    @PostMapping("/{postId}")
+    @PostMapping("/{postId}/like")
     public SuccessResonse likePost(@PathVariable("postId") final Long postId,
                                    @AuthenticatedMemberId final Long memberId) {
         postLikeService.likePost(postId, memberId);
         return SuccessResonse.of();
     }
 
-    @DeleteMapping("{postId}")
+    @DeleteMapping("{postId}/like")
     public SuccessResonse disLikePost(@PathVariable("postId") final Long postId,
                                       @AuthenticatedMemberId final Long memberId) {
         postLikeService.disLikePost(postId, memberId);
