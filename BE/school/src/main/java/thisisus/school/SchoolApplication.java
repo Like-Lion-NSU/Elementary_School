58d8a2bf84ab6897b312299bfdaab404fa398d2a
shyeon4643 <songhyeon4643@gmail.com>
2024-08-28 02:52:45 +0900

    chore : 웹소켓•몽고DB 설정

--- BE/school/src/main/java/thisisus/school/SchoolApplication.java
+++ BE/school/src/main/java/thisisus/school/SchoolApplication.java
@@ -3,14 +3,20 @@ package thisisus.school;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableJpaRepositories
+@EnableMongoRepositories
 @EnableFeignClients
+@EnableCaching
 @ConfigurationPropertiesScan(basePackages = "thisisus.school")
 public class SchoolApplication {
 
